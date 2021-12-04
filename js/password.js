60f67145fb216d6db37489aa0fc119b556b465a6
NoobBit <mmtmdms05@gmail.com>
2021-12-03 20:03:37 -0800

    Added Hashing Capabilities for Password.js

--- js/password.js
+++ js/password.js
@@ -3,13 +3,16 @@ const pass_button = document.getElementById("pass_button")
 pass_button.addEventListener("click", () => {
     const pass_inputbox = document.getElementById("pass_input").value
 
-    if (pass_inputbox == "abc123")
+    const hashed_pass = "a9f6c1ab30610ab70b21d1a3cdc6f001"
+    let hashed_inputbox = md5(pass_inputbox)
+
+    if (hashed_inputbox == hashed_pass)
     {
         console.log("Good")
     }
     else
     {
         console.log("Bad")
-        alert(pass_inputbox)
+        alert(hashed_inputbox)
     }
 })
